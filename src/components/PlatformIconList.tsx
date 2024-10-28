da9167d06a963b22eba6d677f3705e54ea4eceb2
aninozaridze <aninozaridze@gmail.com>
2024-10-28 19:07:03 +0400

    added platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,34 @@
+import {Platform} from '../hooks/useGames'
+import {Icon} from "@chakra-ui/react";
+import {FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid} from 'react-icons/fa';
+import {MdPhoneIphone} from 'react-icons/md';
+import {SiNintendoswitch} from "react-icons/si";
+import {BsGlobe} from 'react-icons/bs'
+import {HStack} from '@chakra-ui/react'
+
+interface Props {
+    platforms: Platform[]
+}
+
+const PlatformIconList = ({platforms}: Props) => {
+    const iconMap: { [key: string] } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendoswitch,
+        mac: FaApple,
+        linux: FaLinux,
+        android: FaAndroid,
+        ios: MdPhoneIphone,
+        web: BsGlobe
+    }
+    console.log(platforms)
+    return (
+        <HStack marginY={'10px'}>
+            {platforms.map(platform =>
+                <Icon as={iconMap[platform.slug]} color='gray.500'/>)}
+        </HStack>
+    );
+};
+
+export default PlatformIconList;

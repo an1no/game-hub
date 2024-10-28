e5b90fc573c381d42e1b7a1bbe29052f6e5193d5
aninozaridze <aninozaridze@gmail.com>
2024-10-28 13:52:30 +0400

    init chakra ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from 'vite-plugin-commonjs';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs(), nodePolyfills()],
 })

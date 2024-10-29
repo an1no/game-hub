82d48950781a3488827b485dbea7b8c72d1c0e40
aninozaridze <aninozaridze@gmail.com>
2024-10-29 17:51:47 +0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import commonjs from 'vite-plugin-commonjs';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
+import checker from 'vite-plugin-checker';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), commonjs(), nodePolyfills()],
+  plugins: [react(), commonjs(), nodePolyfills(), checker({ typescript: false })],
 })

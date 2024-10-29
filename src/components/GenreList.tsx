c9f2bdf43dd4f553b5d52e3e42d5d0d895bd9de6
aninozaridze <aninozaridze@gmail.com>
2024-10-29 19:33:47 +0400

    added genres list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import useGenres, {Genre} from "../hooks/useGenres.ts";
+import useGenres from "../hooks/useGenres.ts";
+import {HStack, List, Text, Image, ListItem} from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url.ts";
 
 const GenreList = () => {
     const {data} = useGenres();
     return (
-        <ul>
-            {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
+        <List>
+            {data.map(genre =>
+                <ListItem key={genre.id} paddingY='5px'>
+                    <HStack>
+                        <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
+                        <Text fontSize='lg'>{genre.name}</Text>
+                    </HStack>
+                </ListItem>)}
+        </List>
     );
 };
 

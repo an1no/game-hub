7aa825ec0f6f8f64e509a4c70deaff7de65bb770
aninozaridze <aninozaridze@gmail.com>
2024-11-13 14:39:45 +0400

    update chakra version

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from 'vite-plugin-commonjs';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
-import checker from 'vite-plugin-checker';
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), checker({typescript: true}), commonjs(), nodePolyfills(),], build: {
-        outDir: 'dist'
-    }, resolve: {
-        dedupe: ['react', 'react-dom'],
-    },
+    plugins: [react()],
 })
